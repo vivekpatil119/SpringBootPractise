1b8ab06db273fe38fcaba839b03a803394e45447
Nextalytics-11 <vivekpatil119@gmail.com>
2019-06-16 00:22:33 +0530

    initial commit

---
+++ spring-boot-drool/src/main/java/com/analyticsfox/MegaOfferController.java
@@ -0,0 +1,26 @@
+package com.analyticsfox;
+
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.analyticsfox.Order;
+
+
+
+@RestController
+@RequestMapping("/megaoffer")
+public class MegaOfferController {
+	@Autowired
+	private KieSession session;
+
+	@PostMapping("/order")
+	public Order orderNow(@RequestBody Order order) {
+		session.insert(order);
+		session.fireAllRules();
+		return order;
+	}
+}
---
+++ spring-boot-drool/src/main/java/com/analyticsfox/SpringBootDroolApplication.java
@@ -0,0 +1,13 @@
+package com.analyticsfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDroolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDroolApplication.class, args);
+	}
+
+}
